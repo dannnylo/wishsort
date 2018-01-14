7db07224d78b90912f16e7da0522238001e973e7
Danilo Jeremias da Silva <daniloj.dasilva@gmail.com>
2018-01-13 23:14:50 -0200

    Added base script

---
+++ wishsort.js
@@ -0,0 +1,52 @@
+var WishSort = {
+  ajaxPages: 0,
+  priceToInteger: function(text){
+    text = text.replace('BRL','').replace('Gratuito','0').replace('R$','');
+    text = text.replace('USD','').replace('Free','0').replace('$','');
+    return parseInt(text, 10);
+  },
+
+  allProductsListed: function(){
+    return $('#feed-more-end').is(':visible');
+  },
+
+  sortProducts: function (){
+    var sorted = $('.feed-product-item').sort(function(prodA, prodB){
+      var priceA = WishSort.priceToInteger($(prodA).find('.feed-actual-price').text());
+      var priceB = WishSort.priceToInteger($(prodB).find('.feed-actual-price').text());
+      return ((priceA < priceB) ? -1 : ((priceA > priceB) ? 1 : 0));
+    });
+
+    $.each(sorted, function(){
+      $(this).appendTo($('#feed-grid'));
+    })
+  },
+
+  getPages: function(limit){
+    if (limit > WishSort.ajaxPages){
+      setTimeout(function(){
+        if (WishSort.allProductsListed()){
+          WishSort.sortProducts();
+        } else{
+          WishSort.getPages(limit);
+        }
+      }, 1000);
+    } else {
+      WishSort.sortProducts();
+    }
+    WishSort.ajaxPages += 1;
+    window.scrollTo(0,99999);
+    return true;
+  },
+
+  sort: function(limit){
+    WishSort.ajaxPages = 0;
+    if (this.allProductsListed()){
+      this.sortProducts();
+    } else {
+      this.getPages(limit);
+    }
+  },
+}
+
+// WishSort.sort(20);
