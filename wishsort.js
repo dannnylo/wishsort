1a417d1e45ea6568da3437c3cb753d7892b3745a
Danilo Jeremias da Silva <daniloj.dasilva@gmail.com>
2020-01-26 18:14:29 -0300

    Sort button

--- wishsort.js
+++ wishsort.js
@@ -1,14 +1,8 @@
 var WishSort = {
-  ajaxPages: 0,
   priceToInteger: function(text){
     text = text.replace('BRL','').replace('Gratuito','0').replace('R$','');
     text = text.replace('USD','').replace('Free','0').replace('$','');
     return parseInt(text, 10);
-  },
-
-  allProductsListed: function(){
-    var noMoreItens = document.querySelectorAll("[class*=ProductGrid__NoItems]")
-    return noMoreItens.length > 0
   },
 
   sortProducts: function (){
@@ -19,48 +13,26 @@ var WishSort = {
       return ((priceA < priceB) ? -1 : ((priceA > priceB) ? 1 : 0));
     });
 
-    var itemsByRow = 4
-
     for (var item in sorted) {
       var itemIndex = parseInt(item, 10)
+
       if (!isNaN(itemIndex)){
-        var row = parseInt(itemIndex / itemsByRow)
-        grid = WishSort.findRow(row)
+        item.style = "border: 1px solid red";
+        grid = WishSort.findRow(parseInt(itemIndex / 4))
         grid.append(sorted[item])
       }
     }
-
-    window.scrollTo(0,0);
-
   },
   findRow(i) {
-    return document.querySelectorAll("[class*=ProductGrid__ProductGridRow]")[i]
+    return WishSort.findByClass('ProductGrid__ProductGridRow')[i]
+    // return WishSort.findByClass('TabBarWrapper__Wrapper')[0]
   },
-
-  getPages: function(limit){
-    WishSort.ajaxPages += 1;
-    window.scrollTo(0,99999);
-    if (limit > WishSort.ajaxPages){
-      setTimeout(function(){
-        if (WishSort.allProductsListed()){
-          setTimeout(WishSort.sortProducts, 1000);
-        } else{
-          WishSort.getPages(limit);
-        }
-      }, 1000);
-    } else {
-      setTimeout(WishSort.sortProducts, 1000);
-    }
-    return true;
-  },
-
-  sort: function(limit){
-    WishSort.ajaxPages = 0;
-    if (this.allProductsListed()){
-      this.sortProducts();
-    } else {
-      this.getPages(limit);
-    }
+  findByClass(name){
+    return document.querySelectorAll("[class*=" + name + "]")
   }
 }
-WishSort.sortProducts()
+
+var button = document.createElement('button');
+button.innerText = 'Sort';
+button.onclick = function() { WishSort.sortProducts(); }
+WishSort.findByClass('Navbar__Right')[0].append(button)
